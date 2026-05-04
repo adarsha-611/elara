4293885688bcc2c4d7a5d21df39ef0e2dfd75669
adarsha-611 <adarshas611@gmail.com>
2026-05-04 11:38:53 +0530

    commit last changes

--- src/config/passport.js
+++ src/config/passport.js
@@ -5,54 +5,52 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      try {
-        let user = await User.findOne({ googleId: profile.id });
+if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          let user = await User.findOne({ googleId: profile.id });
 
-        if (!user) {
-         
-          user = await User.findOne({ email: profile.emails[0].value });
+          if (!user) {
+            user = await User.findOne({ email: profile.emails[0].value });
 
-          if (user) {
-        user.googleId = profile.id;
-        user.authType = "google";   
-        await user.save();
-
-          } else {
-         
-          user = new User({
-          fullName: profile.displayName,
-          email: profile.emails[0].value,
-          googleId: profile.id,
-          authType: "google",   
-          isVerified: true,
-          isBlocked:false 
-});
-            await user.save();
+            if (user) {
+              user.googleId = profile.id;
+              user.authType = "google";
+              await user.save();
+            } else {
+              user = new User({
+                fullName: profile.displayName,
+                email: profile.emails[0].value,
+                googleId: profile.id,
+                authType: "google",
+                isVerified: true,
+                isBlocked: false,
+              });
+              await user.save();
+            }
           }
-        }
 
-        if(user.isBlocked){
-           console.log("USER IS BLOCKED - sending message"); 
-          return done (null,false,{
-            message:"Your account is not accessible.Please contact support."
-          })
+          if (user.isBlocked) {
+            return done(null, false, {
+              message: "Your account is not accessible. Please contact support.",
+            });
+          }
+
+          return done(null, user);
+        } catch (error) {
+          return done(error, null);
         }
-        return done(null, user);
-      } 
-    catch (error) {
-        return done(error, null);
       }
-    }
-  )
-);
+    )
+  );
+}
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -66,5 +64,6 @@ passport.deserializeUser(async (id, done) => {
     done(error, null);
   }
 });
+
 
 export default passport;
